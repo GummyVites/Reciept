9320467e9bbda7b666873f87e09146d55cd30f6d
GummyVites <kemalee@ucsc.edu>
2017-11-13 22:28:58 -0800

    Connected camera with OCR, created progress circle

--- app/src/main/java/com/example/kevinlee/buttonboy/OCRActivity.java
+++ app/src/main/java/com/example/kevinlee/buttonboy/OCRActivity.java
@@ -1,43 +1,104 @@
 package com.example.kevinlee.buttonboy;
 
-import android.content.res.AssetManager;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.util.Base64;
+import java.io.ByteArrayInputStream;
 import com.googlecode.tesseract.android.TessBaseAPI;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import io.netopen.hotbitmapgg.library.view.RingProgressBar;
+
 
 public class OCRActivity extends AppCompatActivity {
-    Bitmap image;
+    public ImageView mImage;
+    public Uri mImageUri;
     private TessBaseAPI mTess;
-    String datapath = "";
     private TessOCR TessOCR;
+    public  Bitmap bitmap;
+    public RingProgressBar mRingProgressBar;
+    int progress = 0;
+
+    Handler myHandler = new Handler()
+    {
+        @Override
+        public void handleMessage (Message msg){
+        if (msg.what == 0) {
+            if (progress < 100) {
+                progress++;
+                mRingProgressBar.setProgress(progress);
+            }
+        }
+    }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocr);
-
         TessOCR = new TessOCR(OCRActivity.this,"eng");
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
 
+            public void onClick(View v) {
+
+                mRingProgressBar = (RingProgressBar) findViewById(R.id.progress_bar_2);
+                mRingProgressBar.setProgress(progress);
+                mRingProgressBar.setOnProgressListener(new RingProgressBar.OnProgressListener(){
+                    @Override
+                    public void progressToComplete()
+                    {
+                        // Progress reaches the maximum callback default Max value is 100
+                        Toast.makeText(OCRActivity.this, "complete", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i =0; i<100; i++){
+                            try{
+                                Thread.sleep(100);
+                                myHandler.sendEmptyMessage(0);
+                            }catch(InterruptedException e){
+                                e.printStackTrace();
+                            }
+                        }
+
+                    }
+                }).start();
+
+            }
+        });
     }
 
     public void processImage(View view){
-        image = BitmapFactory.decodeResource(getResources(), R.drawable.test);
-
-        String temp = TessOCR.getOCRResult(image);
-
+        SharedPreferences shared = getSharedPreferences("MyApp_Settings", MODE_PRIVATE);
+        String photo = shared.getString("PRODUCT_PHOTO", "photo");
+        assert photo != null;
+        if(!photo.equals("photo"))
+        {
+            byte[] b = Base64.decode(photo, Base64.DEFAULT);
+            InputStream is = new ByteArrayInputStream(b);
+            bitmap = BitmapFactory.decodeStream(is);
+        }
+        String temp = TessOCR.getOCRResult(bitmap);
         TextView tv1 = (TextView)findViewById(R.id.textView);
         tv1.setText(temp);
     }
+
 }
--- app/src/main/java/com/example/kevinlee/buttonboy/cameraActivity.java
+++ app/src/main/java/com/example/kevinlee/buttonboy/cameraActivity.java
@@ -2,16 +2,25 @@ package com.example.kevinlee.buttonboy;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.content.Intent;
+import android.util.Base64;
 import android.widget.ImageView;
 import android.view.View;
+import android.content.SharedPreferences;
+import android.net.Uri;
+
+import java.io.ByteArrayOutputStream;
 
 public class cameraActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1888;
+    public ImageView mImage;
+    public Uri mImageUri;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView imageview;
 
     @Override
@@ -29,13 +38,27 @@ public class cameraActivity extends AppCompatActivity {
         }
     }
 
-    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageview.setImageBitmap(imageBitmap);
+            if (data != null) {
+
+                Bitmap picture = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                picture.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                byte[] b = baos.toByteArray();
+                String imageEncoded = Base64.encodeToString(b, Base64.DEFAULT);
+                SharedPreferences shared = getSharedPreferences("MyApp_settings", MODE_PRIVATE);
+                SharedPreferences.Editor editor = shared.edit();
+                editor.putString("PRODUCT_PHOTO", imageEncoded);
+                editor.commit();
+
+
+                // Sets the ImageView with the Image URI
+                startActivity(new Intent(cameraActivity.this, OCRActivity.class));
+            }
         }
     }
+
 
 }
