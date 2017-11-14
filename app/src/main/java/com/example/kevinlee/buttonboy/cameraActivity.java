a91926ef9da648981923518b6b456b3e889039e4
Topher50 <chrisj.evans50@gmail.com>
2017-11-13 17:35:16 -0800

    Fixed camera from crashing

--- app/src/main/java/com/example/kevinlee/buttonboy/cameraActivity.java
+++ app/src/main/java/com/example/kevinlee/buttonboy/cameraActivity.java
@@ -1,6 +1,8 @@
 package com.example.kevinlee.buttonboy;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.widget.ImageView;
 import android.view.View;
 
 public class cameraActivity extends AppCompatActivity {
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     static final int REQUEST_IMAGE_CAPTURE = 1888;
     private ImageView imageview;
@@ -17,9 +20,34 @@ public class cameraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkPermissions();
         setContentView(R.layout.activity_camera);
         dispatchTakePictureIntent();
         this.imageview = (ImageView)this.findViewById(R.id.imageView1);
+    }
+
+    private void checkPermissions() {
+        int hasCameraPermissions = checkSelfPermission(Manifest.permission.CAMERA);
+        if (hasCameraPermissions != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {Manifest.permission.CAMERA},
+                    REQUEST_CODE_ASK_PERMISSIONS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    return;
+                } else {
+                    // Permission Denied
+                    finish();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     private void dispatchTakePictureIntent() {
